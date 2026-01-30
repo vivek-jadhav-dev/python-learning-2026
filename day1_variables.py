be11e1b87b7e182bb1c8e8b78719c5568dc7fe12
Your Name <you@example.com>
2026-01-30 20:24:30 +0530

    Day 1–2: Python basics and conditions

---
+++ day1_variables.py
@@ -0,0 +1,15 @@
+name ="vivek jadhav"
+age = 20 
+height = 5.9
+print(type(height))
+
+#name =input("enter your name :")
+#age =int(input("enter your age :"))
+#print ("hello",name)
+#print("next year you will be ",age +1)
+num1=int(input("enter the first number :"))
+num2 = int(input("enter the second number :"))
+print("sum of two numbers is", num1 + num2)
+print ("differnce between the two numbers is :",num1-num2)
+print ("division of this two nubers is :",num1/num2)
+print("multiplication of this two numbers is:",num1*num2)
